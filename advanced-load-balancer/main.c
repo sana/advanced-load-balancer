e0afdcd94a149e1a039f01e0845ea0cdeb0d5330
Laurentiu Dascalu <dascalu.laurentziu@gmail.com>
2013-12-28 12:23:27 +0200

    Initial Commit

---
+++ advanced-load-balancer/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  advanced-load-balancer
+//
+//  Created by Laurentiu Dascalu on 12/28/13.
+//  Copyright (c) 2013 Laurentiu Dascalu. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
